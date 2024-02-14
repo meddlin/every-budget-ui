7cb07cad494ee7408bef5a5b6000f52dd1520b21
meddlin <drushing.dev@gmail.com>
2024-02-14 02:25:50 -0600

    New categories appended by button
    - Uses some fancy useState array expand and replace logic

--- src/app/budget/page.js
+++ src/app/budget/page.js
@@ -3,54 +3,85 @@
 import Category from './category';
 import { useState, useEffect } from 'react';
 import SearchBar from './search';
+import { replace } from 'formik';
 
-const Budget = () => {
-    const [stateCategories, setStateCategories] = useState(
-        [
+const initialCategories = [
+    {
+        name: 'Income',
+        description: 'Money coming in',
+        planned: 0.00,
+        spent: 0.00,
+        budgetItems: [
             {
-                name: 'Income',
-                description: 'Money coming in',
+                name: 'Paycheck',
+                planned: 0.00,
+                spent: 0.00
+            }
+        ]
+    },
+    {
+        name: 'Housing',
+        description: 'Any regular expense on the house',
+        planned: 0.00,
+        spent: 0.00,
+        budgetItems: [
+            {
+                name: 'Mortgage',
                 planned: 0.00,
                 spent: 0.00,
-                budgetItems: [
+                transactions: [
                     {
-                        name: 'Paycheck',
-                        planned: 0.00,
-                        spent: 0.00
+                        vendor: 'UWM',
+                        amount: 0.00
                     }
                 ]
             },
             {
-                name: 'Housing',
-                description: 'Any regular expense on the house',
+                name: 'Electric',
                 planned: 0.00,
-                spent: 0.00,
-                budgetItems: [
-                    {
-                        name: 'Mortgage',
-                        planned: 0.00,
-                        spent: 0.00,
-                        transactions: [
-                            {
-                                vendor: 'UWM',
-                                amount: 0.00
-                            }
-                        ]
-                    },
-                    {
-                        name: 'Electric',
-                        planned: 0.00,
-                        spent: 0.00
-                    },
-                    {
-                        name: 'Water',
-                        planned: 0.00,
-                        spent: 0.00
-                    }
-                ]
+                spent: 0.00
+            },
+            {
+                name: 'Water',
+                planned: 0.00,
+                spent: 0.00
             }
         ]
-    );
+    }
+];
+
+/**
+ * Constructor function for Category
+ * @returns 
+ */
+const newCategory = () => {
+    return { 
+        name: 'new name', 
+        description: 'new desc', 
+        planned: 100.00, 
+        spent: 21.00,
+        budgetItems: []
+    };
+}
+
+/**
+ * Replace (with) expanded array - This function accepts an array and "constructor" 
+ * function, and returns a new array with pre-existing array contents including
+ * the resulting object from the "constructor".
+ * @param {Existing array to be expanded} arr 
+ * @param {Function to be invoked, which will return a new object matching 
+ * expected schema of objects existing in the array} constructor 
+ * @returns
+ */
+const replExpArr = (arr, constructor) => {
+    if (typeof generator === 'function')
+        return [...arr, constructor()];
+
+    throw new Error(`replExpArr - constructor passed in was not a function. Constructor contents: ${constructor.toString()}`)
+}
+
+const Budget = () => {
+    const [categories, setCategories] = useState(initialCategories);
 
     // const [categories, setCategories] = useState([]);
     // useEffect(() => {
@@ -63,75 +94,33 @@ const Budget = () => {
     //     fetchCategories();
     // }, []);
 
-    // Stub data
-    // const categories = [
-    //     {
-    //         name: 'Income',
-    //         description: 'Money coming in',
-    //         planned: 0.00,
-    //         spent: 0.00,
-    //         budgetItems: [
-    //             {
-    //                 name: 'Paycheck',
-    //                 planned: 0.00,
-    //                 spent: 0.00
-    //             }
-    //         ]
-    //     },
-    //     {
-    //         name: 'Housing',
-    //         description: 'Any regular expense on the house',
-    //         planned: 0.00,
-    //         spent: 0.00,
-    //         budgetItems: [
-    //             {
-    //                 name: 'Mortgage',
-    //                 planned: 0.00,
-    //                 spent: 0.00,
-    //                 transactions: [
-    //                     {
-    //                         vendor: 'UWM',
-    //                         amount: 0.00
-    //                     }
-    //                 ]
-    //             },
-    //             {
-    //                 name: 'Electric',
-    //                 planned: 0.00,
-    //                 spent: 0.00
-    //             },
-    //             {
-    //                 name: 'Water',
-    //                 planned: 0.00,
-    //                 spent: 0.00
-    //             }
-    //         ]
-    //     }
-    // ];
-
     return (
-        <div className="flex content-center justify-center">
-            <SearchBar />
+        <div className="flex flex-col content-center justify-center">
+            <div className="flex">
+                <div className="w-1/4">
+                    {/* left gutter */}
+                </div>
+                <div className="w-1/2">
+                    <SearchBar />
+                
+                    <div>
+                        {categories.map((category, idx) => (
+                            <Category
+                                key={idx}
+                                name={category.name}
+                                description={category.description}
+                                budgetItems={category.budgetItems}
+                            />
+                        ))}
 
-            <div className="w-1/2">
-                {stateCategories.map((category, idx) => (
-                    <Category
-                        key={idx}
-                        name={category.name}
-                        description={category.description}
-                        budgetItems={category.budgetItems}
-                    />
-                ))}
-
-                <button onClick={() => setStateCategories([
-                    ...stateCategories, { 
-                        name: 'new name', 
-                        description: 'new desc', 
-                        planned: 100.00, 
-                        spent: 21.00,
-                        budgetItems: []
-                    }
-                ])}>Add Category</button>
+                        <button onClick={
+                            () => setCategories(replExpArr(categories, newCategory))
+                        }>Add Category</button>
+                    </div>
+                </div>
+                <div className="w-1/4">
+                    {/* right gutter */}
+                </div>
             </div>
         </div>
     )
