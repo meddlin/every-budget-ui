4f9715a4fd11cf4ef056a9d0d31c2b8c35c8f68a
meddlin <drushing.dev@gmail.com>
2024-03-13 02:27:28 -0500

    Transactions display as sensible table

--- src/app/budget/transactions-viewer/transactions-viewer.js
+++ src/app/budget/transactions-viewer/transactions-viewer.js
@@ -1,40 +1,92 @@
-const TransactionsViewer = ({ transactions }) => {
-    const header = transactions && transactions.length > 0 ? Object.keys(transactions[0]) : [];
+import { 
+  useReactTable, 
+  getCoreRowModel,
+  createColumnHelper, 
+  flexRender, 
+} from "@tanstack/react-table";
 
-    return (
-        <>
-            <h2>Transactions</h2>
-            <div className="flex justify-center content-center">
-                <div className="">
-                    <table className="text-sm">
-                        <thead>
-                            <tr>
-                                {header.map((item, key) => (
-                                    <th key={key}>{item}</th>
-                                ))}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {transactions && transactions.length > 0 ? (
-                                transactions.map((rec, idx) => {
-                                    return (
-                                        <tr key={idx}>
-                                            {
-                                                Object.values(rec).map(
-                                                    (val, key) => <td key={key}>{val}</td>
-                                                )
-                                            }
-                                        </tr>
-                                    );
-                                }
-                                )
-                            ) : <tr><td>No transactions available</td></tr>}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </>
-    );
+const columnHelper = createColumnHelper();
+const columns = [
+  columnHelper.accessor("vendor", {
+    header: () => <h3>Vendor</h3>,
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor("amount", {
+    header: () => <h3>Amount</h3>,
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor("transactionDate", {
+    header: () => <h3>Transaction Date</h3>,
+    cell: (info) => {
+      return (
+        new Date(info.getValue()).toLocaleDateString()
+      );
+    },
+  }),
+];
+
+const TransactionsViewer = ({ transactions }) => {
+    // const header = transactions && transactions.length > 0 ? Object.keys(transactions[0]) : [];
+    const table = useReactTable({
+        columns,
+    data: transactions && transactions.length > 0 ? transactions : [],
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <>
+      <h2>Transactions</h2>
+      <div className="flex justify-center content-center">
+        <div className="">
+          <table>
+            <thead>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <tr key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => (
+                    <th
+                      key={header.id}
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      {header.isPlaceholder ? null : (
+                        <>
+                          <div>
+                            {flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                          </div>
+                        </>
+                      )}
+                    </th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            <tbody>
+              {table.getRowModel().rows.map((row) => (
+                <tr
+                  key={row.id}
+                  className="leading-4 text-sm hover:bg-slate-100 hover:cursor-pointer"
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <td
+                      key={cell.id}
+                      className="whitespace-nowrap px-3 py-4 text-sm text-gray-500"
+                    >
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TransactionsViewer;
