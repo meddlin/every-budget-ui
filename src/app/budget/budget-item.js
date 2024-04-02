1bfd4306a190dc04b05d31056a033b9d6e41c4ef
meddlin <drushing.dev@gmail.com>
2024-04-02 03:06:03 -0500

    Drag and drop transactions
    - Basics of drag and drop elements exist
    - react-dnd installed
    - This needs some work.

--- src/app/budget/budget-item.js
+++ src/app/budget/budget-item.js
@@ -21,9 +21,16 @@ import {
 } from './transactions-viewer/transactions-viewer-modal';
 import TransactionsViewer from './transactions-viewer/transactions-viewer';
 import { round } from '../../utility/calculators';
+import { useDroppable } from '@dnd-kit/core';
 
 const BudgetItem = ({ data }) => {
     const { name, planned, spent, transactions } = data;
+    const { isOver, setNodeRef } = useDroppable({
+        id: `droppable-${name}`
+    });
+    const style = {
+        color: isOver ? 'green' : undefined
+    }
 
     /**
      * Note: This rounding logic isn't guaranteed, and needs to be tested.
@@ -49,83 +56,85 @@ const BudgetItem = ({ data }) => {
     };
 
     return (
-        <div className="border-t border-gray-100">
-            <div className="flex px-4 py-6 sm:gap-4 sm:px-6">
+        <div ref={setNodeRef} className={`border-t border-gray-100 ${isOver ? 'bg-lime-200' : ''}`}>
 
-                <div className="flex-col w-full">
-                    <div className="flex"> {/* budget item main */}
-                        <div className="w-3/4 text-sm font-medium text-gray-900">{`BI: ${name}`}</div>
-                        <div className="w-1/4 flex justify-between">
+                <div className="flex px-4 py-6 sm:gap-4 sm:px-6">
 
-                            <div className="flex-col">
-                                <div className="flex justify-between">
-                                    <BudgetItemEditModal>
-                                        <BudgetItemEditModalOpenButton>
-                                            <EditButton />
-                                        </BudgetItemEditModalOpenButton>
-                                        <BudgetItemEditModalContents>
-                                            
-                                            <BudgetItemEditForm 
-                                                data={data} 
-                                            />
-
-                                            <BudgetItemEditModalDismissButton>
-                                                <Button text="Close" />
-                                            </BudgetItemEditModalDismissButton>
-                                        </BudgetItemEditModalContents>
-                                    </BudgetItemEditModal>
-                                    
-                                    <BudgetItemDeleteModal>
-                                        <BudgetItemDeleteModalOpenButton>
-                                            <DeleteButton />
-                                        </BudgetItemDeleteModalOpenButton>
-                                        <BudgetItemDeleteModalContents title="Delete budget item">
-                                            <div className="text-black">
-                                                test content
-                                            </div>
-                                            <BudgetItemDeleteModalDismissButton>
-                                                <Button text="Close" />
-                                            </BudgetItemDeleteModalDismissButton>
-                                        </BudgetItemDeleteModalContents>
-                                    </BudgetItemDeleteModal>
-                                </div>
+                    <div className="flex-col w-full">
+                        <div className="flex"> {/* budget item main */}
+                            <div className="w-3/4 text-sm font-medium text-gray-900">{`BI: ${name}`}</div>
+                            <div className="w-1/4 flex justify-between">
 
                                 <div className="flex-col">
-                                    <div className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                        <MoneyDisplay label="Planned" amount={planned} />
+                                    <div className="flex justify-between">
+                                        <BudgetItemEditModal>
+                                            <BudgetItemEditModalOpenButton>
+                                                <EditButton />
+                                            </BudgetItemEditModalOpenButton>
+                                            <BudgetItemEditModalContents>
+                                                
+                                                <BudgetItemEditForm 
+                                                    data={data} 
+                                                />
+
+                                                <BudgetItemEditModalDismissButton>
+                                                    <Button text="Close" />
+                                                </BudgetItemEditModalDismissButton>
+                                            </BudgetItemEditModalContents>
+                                        </BudgetItemEditModal>
+                                        
+                                        <BudgetItemDeleteModal>
+                                            <BudgetItemDeleteModalOpenButton>
+                                                <DeleteButton />
+                                            </BudgetItemDeleteModalOpenButton>
+                                            <BudgetItemDeleteModalContents title="Delete budget item">
+                                                <div className="text-black">
+                                                    test content
+                                                </div>
+                                                <BudgetItemDeleteModalDismissButton>
+                                                    <Button text="Close" />
+                                                </BudgetItemDeleteModalDismissButton>
+                                            </BudgetItemDeleteModalContents>
+                                        </BudgetItemDeleteModal>
                                     </div>
-                                    <div className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                        <MoneyDisplay label="Spent" amount={transactionsSum(transactions)} />
+
+                                    <div className="flex-col">
+                                        <div className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                            <MoneyDisplay label="Planned" amount={planned} />
+                                        </div>
+                                        <div className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                            <MoneyDisplay label="Spent" amount={transactionsSum(transactions)} />
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
+                            </div>
+                        </div>
+                        <div> {/* budget item transactions */}
+                            {transactions && transactions.length > 0 ? (
+                                <TransactionsViewerModal>
+                                    <TransactionsViewerModalOpenButton>
+                                        <PrimaryButton 
+                                            text="View transactions" 
+                                        />
+                                    </TransactionsViewerModalOpenButton>
+                                    <TransactionsViewerModalContents>
+                                        
+                                        <TransactionsViewer transactions={transactions} />
+
+                                        <TransactionsViewerModalDismissButton>
+                                            <Button text="Close" />
+                                        </TransactionsViewerModalDismissButton>
+                                    </TransactionsViewerModalContents>
+                                </TransactionsViewerModal>
+                            ) : (
+                                <dd className="mt-1 text-sm leading-6 text-gray-700 italic sm:col-span-2 sm:mt-0">No transactions</dd>
+                            )}
                         </div>
                     </div>
-                    <div> {/* budget item transactions */}
-                        {transactions && transactions.length > 0 ? (
-                            <TransactionsViewerModal>
-                                <TransactionsViewerModalOpenButton>
-                                    <PrimaryButton 
-                                        text="View transactions" 
-                                    />
-                                </TransactionsViewerModalOpenButton>
-                                <TransactionsViewerModalContents>
-                                    
-                                    <TransactionsViewer transactions={transactions} />
 
-                                    <TransactionsViewerModalDismissButton>
-                                        <Button text="Close" />
-                                    </TransactionsViewerModalDismissButton>
-                                </TransactionsViewerModalContents>
-                            </TransactionsViewerModal>
-                        ) : (
-                            <dd className="mt-1 text-sm leading-6 text-gray-700 italic sm:col-span-2 sm:mt-0">No transactions</dd>
-                        )}
-                    </div>
                 </div>
 
-            </div>
         </div>
     );
 };
