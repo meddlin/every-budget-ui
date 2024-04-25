079231eecc0bdab518d2e5299de9edb0564b1a23
meddlin <drushing.dev@gmail.com>
2024-04-25 00:25:49 -0500

    New transaction detail modal for sidebar
    - new Banner component too

---
+++ src/app/transactions/actions/transaction-detail.js
@@ -0,0 +1,29 @@
+
+const TransactionDetail = ({ id, vendor, amount, transactionDate, imported }) => {
+    return (
+        <div>
+            <div className="mt-6 border-t border-gray-100">
+                <dl className="divide-y divide-gray-100">
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt className="text-sm font-medium leading-6 text-gray-900">Vendor</dt>
+                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{vendor}</dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt className="text-sm font-medium leading-6 text-gray-900">Amount</dt>
+                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{amount}</dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt className="text-sm font-medium leading-6 text-gray-900">Date</dt>
+                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{transactionDate}</dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt className="text-sm font-medium leading-6 text-gray-900">Imported</dt>
+                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{imported ? 'True' : 'False'}</dd>
+                    </div>
+                </dl>
+            </div>
+        </div>
+    );
+};
+
+export default TransactionDetail;
--- src/components/transactions-side-bar.js
+++ src/components/transactions-side-bar.js
@@ -1,23 +1,47 @@
 import { round } from '../utility/calculators';
 import TransactionsSideBarDisplay from './transactions-sidebar-display';
+import { 
+    TransactionDetailModal, 
+    TransactionDetailModalOpenButton,
+    TransactionDetailModalContents
+} from '@/app/transactions/actions/transaction-detail-modal';
+import Banner from '@/components/banner';
+import TransactionDetail from '@/app/transactions/actions/transaction-detail';
 
 const TransactionsSideBar = ({ transactions }) => {
-
     return (
         <div className="">
             {
                 transactions && transactions.length > 0 ? (
                     transactions.map((txn, key) => {
+                        // onClick={() => alert('clicked transaction')}
                         return (
-                            <div key={key} 
-                                // onClick={() => alert('clicked transaction')}
-                                className="flex flex-col ">
-                                    <TransactionsSideBarDisplay 
-                                        id={txn.id}
-                                        vendor={txn.vendor}
-                                        amount={txn.amount}
-                                        date={txn.transactionDate}
-                                        importedFlag={txn.imported} />
+                            <div key={key} className="flex flex-col ">
+                                <TransactionDetailModal>
+                                    <TransactionDetailModalOpenButton>
+                                        <div>
+                                            <TransactionsSideBarDisplay 
+                                                id={txn.id}
+                                                vendor={txn.vendor}
+                                                amount={txn.amount}
+                                                date={txn.transactionDate}
+                                                importedFlag={txn.imported} 
+                                            />
+                                        </div>
+                                    </TransactionDetailModalOpenButton>
+                                    <TransactionDetailModalContents>
+                                        <div>
+                                            <Banner title="Transaction Details" />
+                                            <TransactionDetail 
+                                                id={txn.id}
+                                                vendor={txn.vendor}
+                                                amount={txn.amount}
+                                                date={txn.transactionDate}
+                                                imported={txn.imported} 
+                                            />
+                                        </div>
+                                    </TransactionDetailModalContents>
+                                </TransactionDetailModal>
                             </div>
                         )
                     })
